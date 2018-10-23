99694058affba3633f0c46b2dbdb02dd70f144b6
Jon Wallace <jon.r.wallace@gmail.com>
2018-10-22 22:28:29 -0700

    not a bad filter for lanes, lightness AND dx-gray

--- pipeline.py
+++ pipeline.py
@@ -8,14 +8,32 @@ from moviepy.editor import VideoFileClip
 
 persp_src = np.float32([[285, 664], [1012, 664], [685, 450], [596, 450]])
 # TODO: The destination of the xform doesn't have to be the same dims as the img, this can actually cause loss of detail near the bottom
-persp_dst = np.float32([[320, 720], [960, 720], [960, 0], [320, 0]])
+td_width = 2560
+ll_targ = td_width * 7 / 16
+rl_targ = td_width * 9 / 16
+td_height = 2560
+persp_dst = np.float32([[ll_targ, td_height], [rl_targ, td_height], [rl_targ, td_height-640], [ll_targ, td_height-640]])
 M = cv2.getPerspectiveTransform(persp_src, persp_dst)
 
 def process(img, mtx, dist):
     # Apply camera calibration
     img = cv2.undistort(img, mtx, dist, None, mtx)
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    h = hls[:,:,0]
+    s = hls[:,:,1]
+    l = hls[:,:,2]
+
+    dx = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    dx = np.uint8(255*dx/np.max(dx))
+
+    img = np.zeros_like(img)
+    img[l > 128] = 255
+    img[dx > 30] = 255
+
     # Apply perspective transform
-    img = cv2.warpPerspective(img, M, dsize=img.shape[1::-1], flags=cv2.INTER_LINEAR)
+    img = cv2.warpPerspective(img, M, dsize=(td_width, td_height), flags=cv2.INTER_LINEAR)
     return img
 
 if __name__ == "__main__":
@@ -27,6 +45,11 @@ if __name__ == "__main__":
         cv2.imwrite(os.path.join("output_images", os.path.basename(name)), img)
 
     for name in glob.glob("*.mp4"):
+        #break
         clip = VideoFileClip(name)
-        xform = clip.fl_image(lambda x:process(x, cal['mtx'], cal['dist']))
+        def pvid(i):
+            chan = process(i, cal['mtx'], cal['dist'])
+            return np.dstack([chan, chan, chan])
+        xform = clip.fl_image(pvid)
         xform.write_videofile(os.path.join("output_videos", name), audio=False)
+        break
