9b01f279032fc6136dbf3c8d8f35cc4f24075d5c
Jon Wallace <jon.r.wallace@gmail.com>
2018-10-23 20:32:07 -0700

    refactored into LaneFinder class

--- pipeline.py
+++ pipeline.py
@@ -16,7 +16,7 @@ persp_dst = np.float32([[ll_targ, td_height], [rl_targ, td_height], [rl_targ, td
 M = cv2.getPerspectiveTransform(persp_src, persp_dst)
 
 #Create triangular convolution window 
-window_width = 40
+window_width = 20
 window = np.mgrid[:window_width // 2]
 window = np.concatenate([window, window[::-1]])
 
@@ -40,90 +40,101 @@ def process(img, mtx, dist):
     # Apply perspective transform
     img = cv2.warpPerspective(img, M, dsize=(td_width, td_height), flags=cv2.INTER_LINEAR)
 
+
+
     conv_img = np.dstack([img, img, img])
-    lc = [ll_targ]
-    lg = [True]# Stores the quality of the match
-    wcl = lc[0]
-    rc = [rl_targ]
-    rg = [True]# Stores the quality of the match
-    wcr = rc[0]
+    fit_len = 200
+    class LaneFinder:
+        def __init__(self, initial_position):
+            self.c = [initial_position] # Record of detected lane centers
+            self.g = [True] # Record of when the lane was detected
+            self.wc = self.c[0] # curent search window center
+            self.dwc = 0 # current dy/dt of search window center
 
-    lfit = [0, 0]
-    rfit = [0, 0]
+            self.fit = [0,0] # last fit line coef.s
+
+        @property
+        def wl(self): # current window lower bound
+            return int(np.clip(self.wc - window_width / 2, 0, td_width - window_width - 1))
+
+        @property
+        def wu(self): # current window upper bound
+            return int(np.clip(self.wc + window_width / 2, window_width, td_width - 1))
+
+        def update_fit(self):
+            mask = self.g[:fit_len]
+            if sum(mask) > fit_len/2:
+                xs = np.array(self.c[:fit_len])
+                ys = np.mgrid[:len(xs)]
+                self.fit = np.polyfit(ys[mask], xs[mask], 1)
+
+        def find_lane(self, conv):
+            self.winconv = conv[self.wl: self.wu]
+            if np.max(self.winconv) > 10:
+                nlc = np.clip(np.argmax(self.winconv) + self.wl, 0, td_width-1)
+                self.g.insert(0, True)
+            else:
+                nlc = self.c[0]
+                self.g.insert(0, False)
+            self.c.insert(0, nlc)
+            return self.g[0], nlc
+
+        def f(self, f): # Apply force
+            self.dwc += f
+
+        def tick(self, dt): # Integrate applied force, reset force accumulator
+            self.wc += self.dwc * dt
+            self.dwc = 0
+
+        def seek_lane(self, strength, other_lane, offset):
+            self.f(strength * (other_lane.c[0] - self.wc + offset))
+
+        def seek_center(self, strength):
+            self.f(strength * (self.c[0] - self.wl - window_width / 2))
+
+    l = LaneFinder(ll_targ)
+    r = LaneFinder(rl_targ)
+    
     for i, row in enumerate(img[::-1], start=1):
+        # Draw seach windows
+        conv_img[len(img)-i,l.wl] = [255,255,0]
+        conv_img[len(img)-i,l.wu] = [255,255,0]
+
+        conv_img[len(img)-i,r.wl] = [255,255,0]
+        conv_img[len(img)-i,r.wu] = [255,255,0]
+        
+        # Convol window with row of pixels, crop the overhang
         conv = np.convolve(window, row)[window_width // 2 - 1:1-window_width // 2]
+
+        # Find the max of the convolution within the search window
+        ret, nlc = l.find_lane(conv)
+        #noise_indicator = np.sum(np.multiply(window-, l.winconv))
+        if ret: conv_img[len(img)-i,int(nlc)] = [0,0,255]
+        ret, nrc = r.find_lane(conv)
+        if ret: conv_img[len(img)-i,int(nrc)] = [0,0,255]
         
-        #Move the window based on the last 20 centers
-        fit_len = 200
-        mask = lg[:fit_len]
-        if sum(mask) > fit_len/2:
-            xs = np.array(lc[:fit_len])
-            ys = np.mgrid[:len(xs)]
-            lfit = np.polyfit(ys[mask], xs[mask], 1)
-            #wcl = lfit[0] * -1 + lfit[1]
+        # Calculate polyfit coefs
+        l.update_fit()
+        r.update_fit()
 
 
+        # Logic to move the search window
+        l.f(-l.fit[0]) # Move based on fit
+        if l.g[0]: # If a lane was found
+            l.seek_center(0.1) # Search window should seek to center it
+        else: # if you can't find anything
+            if r.g[0]: # But the other lane can
+                l.seek_lane(0.1, r, -160) # track with the other lane
 
-        mask = rg[:fit_len]
-        if sum(mask) > fit_len/2:
-            xs = np.array(rc[:fit_len])
-            ys = np.mgrid[:len(xs)]
-            rfit = np.polyfit(ys[mask], xs[mask], 1)
-            #wcr = rfit[0] * -1 + rfit[1]
-        
+        r.f(-r.fit[0])
+        if r.g[0]:
+            r.seek_center(0.1)
+        else: # if you can't find anything
+            if r.g[0]: # But the other lane can
+                r.seek_lane(0.1, l, 160) # track with the other lane
 
-
-        #Convol the row with the window, draw the window
-        wll = int(np.clip(wcl - window_width // 2, 0, td_width-1-window_width))
-        wul = int(np.clip(wcl + window_width // 2, window_width, td_width-1))
-        lwin = conv[wll: wul]
-        conv_img[len(img)-i,wll] = [255,255,0]
-        conv_img[len(img)-i,wul] = [255,255,0]
-        
-        wlr = int(np.clip(wcr - window_width // 2, 0, td_width-1-window_width))
-        wur = int(np.clip(wcr + window_width // 2, window_width, td_width-1))
-        rwin = conv[wlr: wur]
-        conv_img[len(img)-i,wlr] = [255,255,0]
-        conv_img[len(img)-i,wur] = [255,255,0]
-        
-
-        try:
-            if np.max(lwin) > 1:
-                nlc = np.clip(np.argmax(lwin) + wll, 0, td_width-1)
-                conv_img[len(img)-i,int(nlc)] = [0,0,255]
-                lg.insert(0, True)
-            else:
-                nlc = lc[0]
-                lg.insert(0, False)
-            
-            if np.max(rwin) > 1:
-                nrc = np.clip(np.argmax(rwin) + wlr, 0, td_width-1)
-                conv_img[len(img)-i,int(nrc)] = [0,0,255]
-                rg.insert(0, True)
-            else:
-                nrc = rc[0]
-                rg.insert(0, False)
-        except ValueError:
-            pass
-
-        wcl -= lfit[0]
-        if lg[0]:
-            wcl += 0.1*(lc[0] - wll - 20)
-        else:
-            if rg[0]:
-                wcl += 0.1*(rc[0] - wcl - 160)
-
-        wcr -= rfit[0]
-        if rg[0]:
-            wcr += 0.1*(rc[0] - wlr - 20)
-        else:
-            if lg[0]:
-                wcr += 0.1*(lc[0] - wcr + 160)
-
-        lc.insert(0, nlc)
-        rc.insert(0, nrc)
-        #c = np.uint8(255*conv/np.max(conv))[:1280]
-        #conv_img[len(img)-i-1] = np.dstack([c, c, c])
+        l.tick(1)
+        r.tick(1)
 
     return np.array(conv_img)
 
@@ -132,6 +143,8 @@ if __name__ == "__main__":
         cal = pickle.load(f)
 
     for name in glob.glob("test_images/*.jpg"):
+        #if name != r"test_images\test3.jpg": continue
+        print(name)
         img = process(cv2.imread(name), cal['mtx'], cal['dist'])
         cv2.imwrite(os.path.join("output_images", os.path.splitext(os.path.basename(name))[0]+".png"), img)
 
